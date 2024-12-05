5e31b62b762155b785346a7b836c2327206ade77
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-12-05 20:11:09 +0300

    Add project files.

---
+++ GenericOlmayanArrayList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericOlmayanArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*ArrayList koleksiyon = new ArrayList();
+
+            koleksiyon.Add(1);
+            koleksiyon.Add("Enes");
+            koleksiyon.Add(3.5);*/
+
+            degerEkle(5);
+
+            Console.ReadLine();
+        }
+
+        static void degerEkle(int deger)
+        {
+            ArrayList koleksiyon = new ArrayList();
+            koleksiyon.Add(deger);
+
+
+        }
+    }
+}
